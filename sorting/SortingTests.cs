c49b2640ce6ef68250fee3ff453cc18ed8efe9f2
Pablo Gonzalez <pablo.gonzalez@softtek.com>
2018-08-02 01:20:47 -0500

    Sorting - Quicksort

--- sorting/SortingTests.cs
+++ sorting/SortingTests.cs
@@ -4,21 +4,27 @@ using Xunit;
 namespace sorting
 {
     public class SortingTests
-    {
-        private readonly ISorter _sorter;
-
-        public SortingTests()
+    {   
+        [Fact]
+        public void MergeSort_ShouldReturnSortedArray()
         {
-            _sorter = new MergeSort();
+            var sorter = new MergeSort();
+            DoTest(sorter);
         }
 
         [Fact]
-        public void MergeSort_ShouldReturnSortedArray()
+        public void QuickSort_ShouldReturnSortedArray()
+        {
+            var sorter = new QuickSort();
+            DoTest(sorter);
+        }
+        
+        private void DoTest(ISorter sorter) 
         {
             int[] array = new int[] { 38, 27, 43, 3, 9, 82, 10 };
             int[] expectedSortedArray = new int[] { 3, 9, 10, 27, 38, 43, 82 };
 
-            _sorter.Sort(array);
+            sorter.Sort(array);
 
             Assert.Equal(expectedSortedArray, array);
         }
