eda7f8827ec3a9561af15b8b4199be1a07b5e567
Pablo Gonzalez <pablogr.gonzalez@gmail.com>
2019-01-19 01:47:38 -0600

    Array Manipulation: Unit test fixture.
    Signed-off-by: Pablo Gonzalez <pablogr.gonzalez@gmail.com>

---
+++ array-manipulation/ArrayManipulationFixture.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace array_manipulation
+{
+    class ArrayManipulationFixture
+    {
+        private readonly IDictionary<string, TestData> testDrive;
+        public ArrayManipulationFixture()
+        {
+
+        }
+    }
+
+    class TestData
+    {
+        public int N { get; set; }
+        public int[][] Queries { get; set; }
+    }
+}
