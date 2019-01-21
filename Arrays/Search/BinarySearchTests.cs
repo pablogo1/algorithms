966c04605b17cbb1c6c95aa5919c4f2851eed4e6
Pablo Gonzalez <pablo.gonzalez@softtek.com>
2019-01-21 03:28:47 -0600

    Binary search

---
+++ Arrays/Search/BinarySearchTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Arrays.Search
+{
+    public class BinarySearchTests
+    {
+        [Theory]
+        public void BinarySearch_Tests(long[] array, long lookupValue, int exp)
+        {
+            
+        }
+    }
+}
