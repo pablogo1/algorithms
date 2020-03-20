7b3dd31888a1eba5d1fcb8a530c2b5e2fdfd4d7b
Pablo Gonzalez <pablogr.gonzalez@gmail.com>
2020-03-19 19:20:51 -0600

    Two sorted arrays median

--- Arrays/TwoSortedArraysMedian/TwoSortedArraysMedian.cs
+++ Arrays/TwoSortedArraysMedian/TwoSortedArraysMedian.cs
@@ -1,10 +1,61 @@
+using System;
+using System.Diagnostics;
+
 namespace Arrays.TwoSortedArraysMedian
 {
     public class Solution
     {
         public static double FindMedianSortedArrays(int[] nums1, int[] nums2) 
         {
-            return 0.0;
+            // first determine the number of items each of the arrays will contribute
+            // to the left half of the merged array
+            int[] A = nums1;
+            int[] B = nums2;
+
+            if (nums1.Length > nums2.Length)
+            {
+                A = nums2;
+                B = nums1;
+            }
+
+            return FindMedianSortedArrays(A, B, 0, A.Length);
+        }
+
+        private static double FindMedianSortedArrays(int[] A, int[] B, int start, int end)
+        {
+            Console.WriteLine($"start = {start}; end = {end}");
+            
+            int halfLength = (A.Length + B.Length + 1) / 2;
+            int partitionA = (end + start) / 2;
+            int partitionB = halfLength - partitionA;
+
+            Debug.WriteLine($"partitionA = {partitionA}; partitionB = {partitionB}");
+
+            int x = partitionA > 0 ? A[partitionA - 1] : int.MinValue;
+            int xP = partitionA >= A.Length ? int.MaxValue : A[partitionA];
+            int y = partitionB > 0 ? B[partitionB - 1] : int.MinValue;
+            int yP = partitionB >= B.Length ? int.MaxValue : B[partitionB];
+
+            Debug.WriteLine($"x = {x}; xP = {xP}");
+            Debug.WriteLine($"y = {y}; yP = {yP}");
+
+            if (x <= yP && y <= xP)
+            {
+                if ((A.Length + B.Length) % 2 == 0)
+                {
+                    return (Math.Max(x, y) + Math.Min(xP, yP)) / 2.0d;
+                }
+
+                return Math.Max(x, y);
+            }
+            else if (x > yP)
+            {
+                return FindMedianSortedArrays(A, B, start, partitionA);
+            }
+            else
+            {
+                return FindMedianSortedArrays(A, B, partitionA + 1, end);
+            }
         }
     }
 }
--- Arrays/TwoSortedArraysMedian/TwoSortedArraysMedianTests.cs
+++ Arrays/TwoSortedArraysMedian/TwoSortedArraysMedianTests.cs
@@ -5,9 +5,12 @@ namespace Arrays.TwoSortedArraysMedian
 {
     public class TwoSortedArraysMedianTests
     {
-        [Theory]
+        [Theory(DisplayName = "FindMedianSortedArrays")]
         [InlineData(new int[] {1, 2}, new int[] {3}, 2.0)]
         [InlineData(new int[] {1, 2}, new int[] {3, 4}, 2.5)]
+        [InlineData(new int[] {4, 20, 32, 50, 55, 61}, new int[] {1, 15, 22, 30, 70}, 30.0)]
+        [InlineData(new int[] {23, 26, 31, 35}, new int[] {3, 5, 7, 9, 11, 16}, 13.5)]
+        [InlineData(new int[] {23, 26, 31, 35, 36}, new int[] {3, 5, 7, 11}, 23.0)]
         public void FindMedianSortedArraysTests(int[] nums1, int[] nums2, double expectedMedian)
         {
             double actual = Solution.FindMedianSortedArrays(nums1, nums2);
