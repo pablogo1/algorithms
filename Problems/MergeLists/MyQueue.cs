10e35becc08b544db1eda89c65230ddabfbeb313
Pablo Gonzalez <pablogr.gonzalez@gmail.com>
2020-04-01 03:09:32 -0600

    My Queue
    Signed-off-by: Pablo Gonzalez <pablogr.gonzalez@gmail.com>

---
+++ Problems/MergeLists/MyQueue.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Problems.MergeLists
+{
+    public class MyQueue
+    {
+        private const string ERR_QUEUE_EMPTY = "Queue is empty.";
+        private readonly SinglyLinkedList innerList = new SinglyLinkedList();
+
+        public bool IsEmpty => innerList.Count == 0;
+
+        public void Enqueue(int data)
+        {
+            innerList.Append(data);
+        }
+
+        public int Dequeue()
+        {
+            var item = innerList.RemoveFirst();
+
+            if (item is null)
+                throw new InvalidOperationException(ERR_QUEUE_EMPTY);
+
+            return item.Data;
+        }
+
+        public int Peek()
+        {
+            var item = innerList.Head;
+
+            if (item is null)
+                throw new InvalidOperationException(ERR_QUEUE_EMPTY);
+
+            return item.Data;
+        }
+    }
+}
---
+++ Problems/MergeLists/MyQueueTests.cs
@@ -0,0 +1,93 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Problems.MergeLists
+{
+    public class MyQueueTests
+    {
+        private readonly MyQueue myQueue;
+
+        public MyQueueTests()
+        {
+            myQueue = new MyQueue();
+        }
+
+        [Fact(DisplayName = "New_MyQueue_Should_Be_Empty")]
+        public void New_MyQueue_Should_Be_Empty()
+        {
+            myQueue.IsEmpty.Should().BeTrue();
+        }
+
+        [Fact(DisplayName = "Enqueue_Should_Put_Item_At_End_Of_Queue")]
+        public void Enqueue_Should_Put_Item_At_End_Of_Queue()
+        {
+            myQueue.Enqueue(1);
+            myQueue.Peek().Should().Be(1);
+
+            myQueue.Enqueue(2);
+            myQueue.Peek().Should().Be(1);
+        }
+
+        [Fact(DisplayName = "Dequeue_Should_Return_Item_At_The_Head_Of_Queue_And_Remove_It")]
+        public void Dequeue_Should_Return_Item_At_The_Head_Of_Queue_And_Remove_It()
+        {
+            myQueue.Enqueue(1);
+            myQueue.Enqueue(2);
+
+            myQueue.Dequeue().Should().Be(1);
+            myQueue.Peek().Should().Be(2);
+        }
+
+        [Fact(DisplayName = "Dequeue_Should_Throw_InvalidOperationException_When_Queue_Is_Empty")]
+        public void Dequeue_Should_Throw_InvalidOperationException_When_Queue_Is_Empty()
+        {
+            Assert.Throws<InvalidOperationException>(() => myQueue.Dequeue());
+        }
+
+        [Fact(DisplayName = "Peek_Should_Return_Item_At_The_Head_Of_Queue_But_Keeps_It_On_Queue")]
+        public void Peek_Should_Return_Item_At_The_Head_Of_Queue_But_Keeps_It_On_Queue()
+        {
+            myQueue.Enqueue(1);
+
+            myQueue.Peek().Should().Be(1);
+            myQueue.Peek().Should().Be(1);
+
+            myQueue.Enqueue(2);
+            myQueue.Peek().Should().Be(1);
+        }
+
+        [Fact(DisplayName = "Peek_Should_Throw_InvalidOperationException_When_Queue_Is_Empty")]
+        public void Peek_Should_Throw_InvalidOperationException_When_Queue_Is_Empty()
+        {
+            Assert.Throws<InvalidOperationException>(() => myQueue.Peek());
+        }
+
+        [Fact(DisplayName = "IsEmpty_Should_Return_False_After_Item_Has_Been_Enqueued")]
+        public void IsEmpty_Should_Return_False_After_Item_Has_Been_Enqueued()
+        {
+            myQueue.Enqueue(1);
+
+            myQueue.IsEmpty.Should().BeFalse();
+
+            myQueue.Dequeue();
+            myQueue.Enqueue(2);
+
+            myQueue.IsEmpty.Should().BeFalse();
+        }
+
+        [Fact(DisplayName = "IsEmpty_ShouldReturn_True_After_All_Items_Had_Been_DeQueued")]
+        public void IsEmpty_ShouldReturn_True_After_All_Items_Had_Been_DeQueued()
+        {
+            myQueue.Enqueue(1);
+            myQueue.Enqueue(2);
+
+            myQueue.Dequeue();
+            myQueue.Dequeue();
+
+            myQueue.IsEmpty.Should().BeTrue();
+        }
+    }
+}
